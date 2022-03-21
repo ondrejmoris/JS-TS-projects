4fbb31ec74380f9a0a129563806f10a1541c0848
Ondřej Moris <moris.o@seznam.cz>
2022-03-21 23:38:56 +0100

    Form and db connect

--- expressTest/src/index.ts
+++ expressTest/src/index.ts
@@ -1,16 +1,41 @@
+/* eslint-disable import/prefer-default-export */
 import express from "express";
 import path from "path";
+import prisma from "./instances";
 import { router as articlesRouter } from "./routes/articles";
 
 const app = express();
 
+async function main() {
+  // ... you will write your Prisma Client queries here
+
+  const allUsers = await prisma.article.findMany();
+
+  console.log(allUsers);
+}
+
+main().catch((error) => console.log(error));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.urlencoded({ extended: false }));
 app.use("/articles", articlesRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articless = [
+    {
+      title: "Article1",
+      createdAt: new Date(),
+      description: "This is test descrtipiton",
+    },
+    {
+      title: "Article2",
+      createdAt: new Date(),
+      description: "This is test descrtipiton 2",
+    },
+  ];
+  res.render("articles/index", { articles: articless });
 });
 
 app.listen(5000);
--- expressTest/src/routes/articles.ts
+++ expressTest/src/routes/articles.ts
@@ -1,8 +1,37 @@
 /* eslint-disable import/prefer-default-export */
 import express from "express";
+import prisma from "../instances";
 
 export const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Articles");
+router.get("/new", (req, res) => {
+  res.render("articles/new", {
+    article: { title: undefined, description: undefined, markdown: undefined },
+  });
+});
+
+// router.get(":id", (req, res) => {});
+
+router.post("/", async (req, res) => {
+  const articleForm = {
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  };
+  try {
+    console.log(req.body.title);
+    console.log(typeof req.body.description);
+    const articleDb = await prisma.article.create({
+      data: {
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+      },
+    });
+    res.redirect(`/articles/${articleDb.id}`);
+  } catch (createError) {
+    res.render("articles/new", {
+      article: articleForm,
+    });
+  }
 });
